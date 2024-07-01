9ca338fd3ea593081547ba7c2d551b3637e5bf9d
Sol Park <solpark16@gmail.com>
2024-07-01 20:01:40 +0900

    Feat: 메인 페이지 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import PokemonList from "@/components/PokemonList";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>하하</div>;
+  return (
+    <div>
+      <h1>포켓몬 도감</h1>
+      <PokemonList />
+    </div>
+  );
 }
